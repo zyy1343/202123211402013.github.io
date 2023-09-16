07b31182b89a01a88488339478adb04df3bf00f0
zyy1343 <144105310+zyy1343@users.noreply.github.com>
2023-09-16 21:34:00 +0800

    Update webgl-utils.js

--- test1/demo02/js/common/webgl-utils.js
+++ test1/demo02/js/common/webgl-utils.js
@@ -28,7 +28,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
+//
 /**
  * @fileoverview This file contains functions every webgl program will need
  * a version of one way or another.
